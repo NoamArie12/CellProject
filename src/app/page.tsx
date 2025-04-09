f0dc8c2d12829f632695071678e613d139caaa0d
noam.arie <noamhazilon12@gmail.com>
2025-04-09 20:35:12 +0300

    Add the all essential things

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,103 +1,62 @@
-import Image from "next/image";
+import Image from 'next/image';
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+    <div className="text-right">
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+      <div className="flex justify-center sm:justify-end w-full mb-10 sm:mb-10 mt-7">
+        <h1 className="text-4xl sm:text-[3rem] sm:mr-7">תא הזרע</h1>
+      </div>
+
+      <div className="w-full flex flex-col sm:flex-row-reverse items-center">
+
+        <div className="mx-5">
+          <p className="text-right">
+            תא הזרע מותאם לתפקידו – הפריית הביצית – באמצעות מבנהו הייחודי, שמאפשר לו לנוע ביעילות, לשרוד בסביבת מערכת הרבייה הנשית ולחדור לביצית.
+          </p>
+          <br />
+          <div className="flex flex-row-reverse gap-2">
+            <span>1.</span>
+            <p className="flex-1 text-right">
+              הראש – מכיל את החומר התורשתי (דנ"א) של האב, אשר יועבר לביצית בעת ההפריה. הדנ"א נמצא בתוך גרעין דחוס במיוחד, מה שמאפשר לתא הזרע להיות קטן ואווירודינמי לניידות טובה יותר.
+            </p>
+          </div>
+          <div className="flex flex-row-reverse gap-2">
+            <span>2.</span>
+            <p className="flex-1 text-right">
+              האקרוזום – זהו כיס מיוחד בראש תא הזרע המכיל אנזימים מפרקי חלבונים וסוכרים. האנזימים הללו מסייעים לפירוק מעטפת הביצית, כדי לאפשר לזרע לחדור לתוכה ולהפרות אותה.
+            </p>
+          </div>
+          <div className="flex flex-row-reverse gap-2">
+            <span>3.</span>
+            <p className="flex-1 text-right">
+              האזור האמצעי (צוואר התא) – מכיל מספר רב של מיטוכונדריה, האברונים האחראים על ייצור אנרגיה. אנרגיה זו חיונית לתנועה ממושכת של הזרע בתוך מערכת הרבייה הנשית בדרכו לביצית.
+            </p>
+          </div>
+          <div className="flex flex-row-reverse gap-2">
+            <span>4.</span>
+            <p className="flex-1 text-right">
+              השוטון (הזנב) – מבנה ארוך ודק הפועל כמו שוט ומייצר תנועתיות. השוטון מתנועע בתנועה גלית שמאפשרת לתא הזרע לשחות קדימה לעבר הביצית.
+            </p>
+          </div>
+          <br />
+          <p className="text-right">
+            מבנה זה מאפשר לתא הזרע לשרוד בסביבה עוינת יחסית, לנוע ביעילות, לחדור את מעטפת הביצית ולהעביר את החומר התורשתי להפריה מוצלחת.
+          </p>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+
+        <div className='m-5 sm:my-0'>
           <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
+          className='rounded-2xl border-2 border-[#171717] dark:border-[#9f9f9f]'
+          src="/images/SpermnCell.jpeg"
+          alt="Science formula image"
+          width={500}
+          height={300}
           />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+        </div>
+
+      </div>
+
     </div>
   );
 }
