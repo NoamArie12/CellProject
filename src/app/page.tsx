fdf391cefaec69594908bbef3b10132d19fa2559
noam.arie <noamhazilon12@gmail.com>
2025-04-13 18:25:36 +0100

    Fix the Home page.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,13 +3,11 @@ import Image from 'next/image';
 export default function Home() {
   return (
     <div className="text-right">
-
       <div className="flex justify-center sm:justify-end w-full mb-10 sm:mb-10 mt-7">
         <h1 className="text-4xl sm:text-[3rem] sm:mr-7">תא הזרע</h1>
       </div>
 
       <div className="w-full flex flex-col sm:flex-row-reverse items-center">
-
         <div className="mx-5">
           <p className="text-right">
             תא הזרע מותאם לתפקידו – הפריית הביצית – באמצעות מבנהו הייחודי, שמאפשר לו לנוע ביעילות, לשרוד בסביבת מערכת הרבייה הנשית ולחדור לביצית.
@@ -18,7 +16,7 @@ export default function Home() {
           <div className="flex flex-row-reverse gap-2">
             <span>1.</span>
             <p className="flex-1 text-right">
-              הראש – מכיל את החומר התורשתי (דנ"א) של האב, אשר יועבר לביצית בעת ההפריה. הדנ"א נמצא בתוך גרעין דחוס במיוחד, מה שמאפשר לתא הזרע להיות קטן ואווירודינמי לניידות טובה יותר.
+              הראש – מכיל את החומר התורשתי (דנא) של האב, אשר יועבר לביצית בעת ההפריה. הדנא נמצא בתוך גרעין דחוס במיוחד, מה שמאפשר לתא הזרע להיות קטן ואווירודינמי לניידות טובה יותר.
             </p>
           </div>
           <div className="flex flex-row-reverse gap-2">
@@ -45,18 +43,16 @@ export default function Home() {
           </p>
         </div>
 
-        <div className='m-5 sm:my-0'>
+        <div className="m-5 sm:my-0">
           <Image
-          className='rounded-2xl border-2 border-[#171717] dark:border-[#9f9f9f]'
-          src="/images/SpermnCell.jpeg"
-          alt="Science formula image"
-          width={500}
-          height={300}
+            className="rounded-2xl border-2 border-[#171717] dark:border-[#9f9f9f]"
+            src="/images/SpermnCell.jpeg"
+            alt="Science formula image"
+            width={500}
+            height={300}
           />
         </div>
-
       </div>
-
     </div>
   );
 }
